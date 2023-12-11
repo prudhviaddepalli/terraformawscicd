e3fd44389a5d9e4c002d95e08dcc810e26b6cc71
prudhviaddepalli <115047399+prudhviaddepalli@users.noreply.github.com>
2023-12-11 11:06:45 -0600

    Update confluent.tf

--- terraform/confluent.tf
+++ terraform/confluent.tf
@@ -10,7 +10,17 @@ terraform {
       source  = "confluentinc/confluent"
       version = "1.55.0"
     }
+
+   azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.55.0"
+    }
   }
+  backend "azurerm" {
+    storage_account_name = "terraformcicd"
+    container_name       = "terraformcicd"
+  }
+  
 }
 
 provider "confluent" {
