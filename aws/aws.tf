14adde7686fa533799e275a6f41c088b00112fb4
prudhviaddepalli <115047399+prudhviaddepalli@users.noreply.github.com>
2023-12-11 10:52:39 -0600

    Update aws.tf

--- aws/aws.tf
+++ aws/aws.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_version = ">= 0.14.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.17.0"
+    }
+    confluent = {
+      source  = "confluentinc/confluent"
+      version = "1.55.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
