f09f17d8c3cef7070e2cf307513a0002816eb5ab
prudhviaddepalli <115047399+prudhviaddepalli@users.noreply.github.com>
2023-12-12 19:17:43 -0600

    Create variables.tf

---
+++ devops/env/network/variables.tf
@@ -0,0 +1,14 @@
+variable "environment" {
+    type = string
+    description = "environment id"
+}
+
+variable "region" {
+  type = string
+  description = "region"
+}
+
+variable "aws_account_id" {
+  description = "The AWS Account ID (12 digits)"
+  type        = string
+}
