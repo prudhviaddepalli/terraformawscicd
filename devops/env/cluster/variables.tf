e82df6517b4e772fc4a7cd8758f9564053fdba89
prudhviaddepalli <115047399+prudhviaddepalli@users.noreply.github.com>
2023-12-13 09:58:49 -0600

    Update variables.tf

--- devops/env/cluster/variables.tf
+++ devops/env/cluster/variables.tf
@@ -1,3 +1,13 @@
+variable "confluent_cloud_api_key" {
+  type        = string
+  description = "Confluent Cloud API KEY. export TF_VAR_confluent_cloud_api_key=\"API_KEY\""
+}
+
+variable "confluent_cloud_api_secret" {
+  type        = string
+  description = "Confluent Cloud API Secret. export TF_VAR_confluent_cloud_api_secret=\"API_SECRET\""
+}
+
 variable "environment" {
     type = string
     description = "environment id"
