b8071dc1bf14adc9c5e7048ec651c102fa7a163b
prudhviaddepalli <115047399+prudhviaddepalli@users.noreply.github.com>
2023-12-12 18:12:22 -0600

    Update confluent.tf

--- terraform/confluent.tf
+++ terraform/confluent.tf
@@ -80,5 +80,19 @@ resource "confluent_private_link_access" "aws" {
   }
 }
 
-
+resource "confluent_kafka_cluster" "dedicated" {
+  display_name = "Privatelink"
+  availability = "SINGLE_ZONE"
+  cloud        = confluent_network.private-link.cloud
+  region       = confluent_network.private-link.region
+  dedicated {
+    cku = 1
+  }
+  environment {
+    id = confluent_environment.staging.id
+  }
+  network {
+    id = confluent_network.private-link.id
+  }
+}
 
