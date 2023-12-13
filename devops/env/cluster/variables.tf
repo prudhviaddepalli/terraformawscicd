1e42be67bed76b0f91d3ddacade2a4285234bb34
prudhviaddepalli <115047399+prudhviaddepalli@users.noreply.github.com>
2023-12-13 09:55:23 -0600

    Update variables.tf

--- devops/env/cluster/variables.tf
+++ devops/env/cluster/variables.tf
@@ -7,3 +7,18 @@ variable "network_id" {
     type = string
     description = "network id for the cluster"
 }
+
+variable "cluster_name" {
+    type = string
+    description = "Name of the new cluster"
+}
+
+variable "cluster_availability" {
+    type = string
+    description = "cluster availability either SINGLE_ZONE or MULTI_ZONE"
+}
+
+variable "ckus" {
+    type = string
+    description = "number of CKU's minimum 2 if MULTI_ZONE"
+}
