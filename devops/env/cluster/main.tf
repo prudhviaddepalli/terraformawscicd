f83e96ee662400dfbf2e32fd5e43eeb611420ea8
prudhviaddepalli <115047399+prudhviaddepalli@users.noreply.github.com>
2023-12-12 19:19:21 -0600

    Create main.tf

---
+++ devops/env/cluster/main.tf
@@ -0,0 +1,22 @@
+data "confluent_environment" "main" {
+  id = var.environment
+}
+
+
+
+
+resource "confluent_kafka_cluster" "dedicated" {
+  display_name = "Privatelink"
+  availability = "SINGLE_ZONE"
+  cloud        = confluent_network.private-link.cloud
+  region       = confluent_network.private-link.region
+  dedicated {
+    cku = 1
+  }
+  environment {
+    id = confluent_environment.staging.id
+  }
+  network {
+    id = confluent_network.private-link.id
+  }
+}
