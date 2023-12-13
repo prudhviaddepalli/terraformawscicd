c0ce7f12f7c8ae693bc73bd07fae00084de9d278
prudhviaddepalli <115047399+prudhviaddepalli@users.noreply.github.com>
2023-12-13 11:53:42 -0600

    Update output.tf

--- devops/env/cluster/output.tf
+++ devops/env/cluster/output.tf
@@ -1,3 +1,24 @@
+output "cluster_displayname" {
+  value = confluent_kafka_cluster.dedicated.display_name
+}
+
 output "bootstrap_endpoint" {
   value = confluent_kafka_cluster.dedicated.bootstrap_endpoint
 }
+
+output "rest_endpoint" {
+  value = confluent_kafka_cluster.dedicated.rest_endpoint
+}
+
+output "rbac_crn" {
+  value = confluent_kafka_cluster.dedicated.rbac_crn
+}
+
+output "env_id" {
+  value = confluent_kafka_cluster.dedicated.environment.id
+}
+
+output "network_id" {
+  value = confluent_kafka_cluster.dedicated.network.id
+}
+
