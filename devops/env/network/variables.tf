b9c2eb2878b36893deca21fdcdeb0f81f3496b0a
prudhviaddepalli <115047399+prudhviaddepalli@users.noreply.github.com>
2023-12-13 10:48:14 -0600

    Update variables.tf

--- devops/env/network/variables.tf
+++ devops/env/network/variables.tf
@@ -12,3 +12,20 @@ variable "aws_account_id" {
   description = "The AWS Account ID (12 digits)"
   type        = string
 }
+
+variable "network_name" {
+  description = "The Network name"
+  type        = string
+}
+
+variable "network_cloud" {
+  description = "The cloud provider for network like AWS, GCP, AZURE"
+  type        = string
+  default     = "AWS"
+}
+
+variable "network_region" {
+  description = "The network region like us-east-1"
+  type        = string
+  default     = "us-east-1"
+}
