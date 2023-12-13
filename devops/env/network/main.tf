92c44155ce92a67c929287c3f7ed1430b85c8d69
prudhviaddepalli <115047399+prudhviaddepalli@users.noreply.github.com>
2023-12-13 10:46:06 -0600

    Update main.tf

--- devops/env/network/main.tf
+++ devops/env/network/main.tf
@@ -1,16 +1,44 @@
+terraform {
+  required_version = ">= 0.14.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.17.0"
+    }
+    confluent = {
+      source  = "confluentinc/confluent"
+      version = "1.55.0"
+    }
+
+   azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.55.0"
+    }
+  }
+  backend "azurerm" {
+    storage_account_name = "terraformcicd"
+    container_name       = "terraformcicd"
+  }
+  
+}
+
+provider "confluent" {
+  cloud_api_key    = var.confluent_cloud_api_key
+  cloud_api_secret = var.confluent_cloud_api_secret
+}
 
 data "confluent_environment" "env" {
   id = var.environment
 }
 
 resource "confluent_network" "private-link" {
-  display_name     = "Private Link Network"
-  cloud            = "AWS"
-  region           = var.region
+  display_name     = var.network_name
+  cloud            = var.network_cloud
+  region           = var.network_region
   connection_types = ["PRIVATELINK"]
   zones            = keys(var.subnets_to_privatelink)
   environment {
-    id = confluent_environment.env.id
+    id = data.confluent_environment.env.id
   }
   dns_config {
     resolution = "PRIVATE"
@@ -23,7 +51,7 @@ resource "confluent_private_link_access" "aws" {
     account = var.aws_account_id
   }
   environment {
-    id = confluent_environment.env.id
+    id = data.confluent_environment.env.id
   }
   network {
     id = confluent_network.private-link.id
