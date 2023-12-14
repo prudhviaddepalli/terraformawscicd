e6adddf6ad1bdb2d302cdd452748986a4da2ae27
prudhviaddepalli <115047399+prudhviaddepalli@users.noreply.github.com>
2023-12-14 14:15:58 -0600

    Update main.tf

--- devops/env/cluster/topics/main.tf
+++ devops/env/cluster/topics/main.tf
@@ -1,1 +1,12 @@
 
+resource "confluent_kafka_topic" "orders" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.dedicated.id
+  }
+  topic_name    = "orders"
+  rest_endpoint = confluent_kafka_cluster.dedicated.rest_endpoint
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
+}
