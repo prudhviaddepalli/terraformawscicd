62196c649730d5ce7c545ba8dcfde8ca0cb52393
prudhviaddepalli <115047399+prudhviaddepalli@users.noreply.github.com>
2023-12-19 10:47:27 -0600

    Update aws.tf

--- aws/aws.tf
+++ aws/aws.tf
@@ -89,7 +89,7 @@ resource "aws_security_group" "privatelink" {
 }
 
 data "aws_subnets" "filtered" {
-  for_each = toset(data.aws_availability_zones.privatelink.zone_ids)
+  for_each = toset(data.aws_availability_zones.privatelink.zones)
 
   filter {
     name   = "availability-zone-id"
